0802da7341fe1a069f1c20f674859e7a7e37ea02
hairybreeches <dc345@cantab.net>
2013-07-23 16:39:23 +0100

    Run the user's code!

--- public/application.js
+++ public/application.js
@@ -1,9 +1,9 @@
 var our = (function () {
     var ret = {};
 
+    var editor = ace.edit("editor");
 
     var initialiseSyntaxHighlighting = function () {
-        var editor = ace.edit("editor");
         editor.setTheme("ace/theme/monokai");
         editor.getSession().setMode("ace/mode/javascript");
     };
@@ -20,7 +20,7 @@ var our = (function () {
 
         var script   = document.createElement("script");
         script.type  = "text/javascript";
-        script.text  = "alert('voila!');";
+        script.text  = editor.getSession().getValue();
 
         var theFrame = document.getElementById("executionFrame").contentWindow; // alternatively: window.frames[0]
         theFrame.document.body.appendChild(script);
