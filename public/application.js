9e7e8ffdcf2bddf38547433852da819700de5992
hairybreeches <dc345@cantab.net>
2013-07-24 17:24:50 +0100

    run the tests (we'll add a debug checkbox later, and we'll add another tab that doesn't have jasmine crammed into it + that allows HTML later too)

--- public/application.js
+++ public/application.js
@@ -25,6 +25,48 @@ var our = (function () {
     initialiseTowTruck();
 
 
+    var createElement = function (parentElement, tag, params) {
+        var element = document.createElement(tag);
+        $.extend(element, params);
+        parentElement.appendChild(element);
+    };
+
+    var injectCssSrc = function (parentElement, source, callback) {
+        createElement(parentElement, "link", {
+            media: "all",
+            rel: "stylesheet",
+            type: "text/css",
+            href: source,
+            onload: callback
+        });
+    };
+
+    var injectScriptSrc = function (parentElement, source, callback) {
+        createElement(parentElement, "script", {
+            type: "text/javascript",
+            src: source,
+            onload: callback
+        });
+    };
+
+    var injectScriptText = function (parentElement, value, callback) {
+        createElement(parentElement, "script", {
+            type: "text/javascript",
+            text: value,
+            onload: callback
+        });
+    };
+
+    var injectEval = function (frameWindow, value, callback) {
+        frameWindow.eval(value);
+        callback();
+    };
+
+    var dispatch = function (values) {
+        values[0](function () { dispatch(values.slice(1)); });
+    };
+
+
     ret.onButtonClick = function () {
 
 
@@ -41,12 +83,22 @@ var our = (function () {
         newFrame.id = id;
 
         newFrame.onload = function () {
-            var text = 'debugger;';
+            var text = '';
             for (var i in editors)
             {
                 text += '\n\n' + editors[i].getSession().getValue();
             }
-            newFrame.contentWindow.eval(text);
+
+
+            var frameWindow = newFrame.contentWindow;
+            var body = frameWindow.document.body;
+            dispatch([
+                function(callback) { injectCssSrc(body, "http://cdn.jsdelivr.net/jasmine/1.3.1/jasmine.css", callback); },
+                function(callback) { injectScriptSrc(body, "http://cdn.jsdelivr.net/jasmine/1.3.1/jasmine.js", callback); },
+                function(callback) { injectEval(frameWindow, text, callback); },
+                function(callback) { injectScriptSrc(body, "http://cdn.jsdelivr.net/jasmine/1.3.1/jasmine-html.js", callback); },
+                function(callback) { injectScriptText(body, "(function () { var env = jasmine.getEnv(); env.addReporter(new jasmine.HtmlReporter()); env.execute(); })();", callback); }
+            ]);
         };
 
         container.appendChild(newFrame);
